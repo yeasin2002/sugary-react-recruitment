e6d3accd1c002d9bf8ec9aee5aa5b866fcae83a8
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-05-09 05:38:03 +0600

    feat: added axios and config with interceptors

---
+++ lib/apiClient.ts
@@ -0,0 +1,119 @@
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  InternalAxiosRequestConfig,
+} from "axios";
+
+let accessToken: string = "";
+let refreshToken: string = "";
+
+export const setTokens = (access: string, refresh: string) => {
+  accessToken = access;
+  refreshToken = refresh;
+};
+
+interface FailedRequest {
+  resolve: (token: string) => void;
+  reject: (error: AxiosError) => void;
+}
+
+interface RefreshTokenResponse {
+  AccessToken: string;
+  RefreshToken: string;
+}
+
+interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
+  _retry?: boolean;
+}
+
+const api: AxiosInstance = axios.create({
+  baseURL: "https://sugarytestapi.azurewebsites.net/",
+});
+
+api.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+let isRefreshing = false;
+let failedQueue: FailedRequest[] = [];
+
+const processQueue = (error: AxiosError | null, token: string | null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else if (token) {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error: AxiosError) => {
+    const originalRequest = error.config as CustomAxiosRequestConfig;
+
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      refreshToken
+    ) {
+      originalRequest._retry = true;
+
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({
+            resolve: (token: string) => {
+              originalRequest.headers = {
+                ...originalRequest.headers,
+                Authorization: `Bearer ${token}`,
+              };
+              resolve(api(originalRequest));
+            },
+            reject,
+          });
+        });
+      }
+
+      isRefreshing = true;
+
+      try {
+        const { data } = await axios.post<RefreshTokenResponse>(
+          "https://sugarytestapi.azurewebsites.net/Account/RefreshToken",
+          {
+            AccessToken: accessToken,
+            RefreshToken: refreshToken,
+          }
+        );
+
+        const newAccessToken = data.AccessToken;
+        const newRefreshToken = data.RefreshToken;
+
+        setTokens(newAccessToken, newRefreshToken);
+        processQueue(null, newAccessToken);
+
+        originalRequest.headers = {
+          ...originalRequest.headers,
+          Authorization: `Bearer ${newAccessToken}`,
+        };
+
+        return api(originalRequest);
+      } catch (err) {
+        processQueue(err as AxiosError, null);
+        return Promise.reject(err);
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
