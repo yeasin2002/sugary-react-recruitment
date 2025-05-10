89bbceeac085014901aeaec491d10a97345f2fae
yeasin2002 <mdkawsarislam2002@gmail.com>
2025-05-11 05:52:08 +0600

    fix: spacing issues

--- components/dashboard/materials-list.tsx
+++ components/dashboard/materials-list.tsx
@@ -52,7 +52,7 @@ export function MaterialsList() {
   }, [inView, loadMoreMaterials]);
 
   return (
-    <div>
+    <div className="pb-40">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 min-h-[80vh]">
         {materials.map((material) => (
           <MaterialCard key={material.Id} material={material} />
